4cbf94da0751fdca2cdea573c59aa1b5c72f6e9a
algoritmika <info@algoritmika.com>
2022-11-28 02:33:26 +0200

    v1.7.0

--- includes/class-alg-awesome-shortcodes.php
+++ includes/class-alg-awesome-shortcodes.php
@@ -56,20 +56,15 @@ final class Alg_Awesome_Shortcodes {
 	 */
 	function __construct() {
 
-		// Save version
-		if ( is_admin() && get_option( 'alg_awesome_shortcodes_version', '' ) !== $this->version ) {
-			update_option( 'alg_awesome_shortcodes_version', $this->version );
-		}
-
 		// Set up localisation
 		add_action( 'init', array( $this, 'localize' ) );
 
-		// Core
-		$this->core = require_once( 'class-alg-awesome-shortcodes-core.php' );
+		// Include required files
+		$this->includes();
 
-		// Action Links
+		// Admin
 		if ( is_admin() ) {
-			add_filter( 'plugin_action_links_' . plugin_basename( ALG_AWESOME_SHORTCODES_FILE ), array( $this, 'action_links' ) );
+			$this->admin();
 		}
 
 	}
@@ -85,6 +80,32 @@ final class Alg_Awesome_Shortcodes {
 	}
 
 	/**
+	 * includes.
+	 *
+	 * @version 1.7.0
+	 * @since   1.7.0
+	 */
+	function includes() {
+		// Core
+		$this->core = require_once( 'class-alg-awesome-shortcodes-core.php' );
+	}
+
+	/**
+	 * admin.
+	 *
+	 * @version 1.7.0
+	 * @since   1.7.0
+	 */
+	function admin() {
+		// Action links
+		add_filter( 'plugin_action_links_' . plugin_basename( ALG_AWESOME_SHORTCODES_FILE ), array( $this, 'action_links' ) );
+		// Version update
+		if ( get_option( 'alg_awesome_shortcodes_version', '' ) !== $this->version ) {
+			add_action( 'admin_init', array( $this, 'version_updated' ) );
+		}
+	}
+
+	/**
 	 * Show action links on the plugin screen.
 	 *
 	 * @version 1.0.0
@@ -96,6 +117,16 @@ final class Alg_Awesome_Shortcodes {
 	function action_links( $links ) {
 		$custom_links = array( '<a href="' . admin_url( 'options-general.php?page=awesome-shortcodes' ) . '">' . __( 'Settings', 'awesome-shortcodes' ) . '</a>' );
 		return array_merge( $custom_links, $links );
+	}
+
+	/**
+	 * version_updated.
+	 *
+	 * @version 1.7.0
+	 * @since   1.7.0
+	 */
+	function version_updated() {
+		update_option( 'alg_awesome_shortcodes_version', $this->version );
 	}
 
 	/**
